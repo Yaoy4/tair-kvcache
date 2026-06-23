0db26084f1e50befb02f717a6723129b39273216
Yaoy4 <yao1.yao@intel.com>
2026-06-23 14:09:17 +0800

    Test PD disaggregate with P=H100 and D=RTX6000

--- hisim/src/hisim/spec/accelerator/info.py
+++ hisim/src/hisim/spec/accelerator/info.py
@@ -2,9 +2,30 @@ from hisim.spec.accelerator.base import AcceleratorInfo
 
 
 class NVIDIA:
+    NVIDIA_H100_SXM = AcceleratorInfo.from_dict(
+        config={
+            "name": "h100_sxm",
+            "device_alias": ["H100_SXM", "h100"],
+            "tflops": {
+                "FP8_TENSOR": 4667,
+                "INT8_TENSOR": 4667,
+                "FP16_TENSOR": 2333,
+                "BF16_TENSOR": 2333,
+                "FP32": 1167,
+            },
+            "hbm_capacity_gb": 80,
+            "hbm_bandwidth_gb": 3350,
+            "inter_node_bandwidth_gb": 64,
+            "intra_node_bandwidth_gb": 450,
+            "vendor": "NVIDIA",
+            "ref": "https://www.nvidia.com/en-us/data-center/h100/",
+        },
+        save_to_registry=True,
+    )
+
     NVIDIA_H20 = AcceleratorInfo.from_dict(
         config={
-            "name": "NVIDIA H20",
+            "name": "h20",
             "device_alias": ["H20", "h20_sxm"],
             "tflops": {
                 "FP8_TENSOR": 296,
@@ -25,9 +46,8 @@ class NVIDIA:
 
     NVIDIA_RTX_PRO_6000_SERVER = AcceleratorInfo.from_dict(
         config={
-            "name": "NVIDIA RTX PRO 6000 Server",
+            "name": "rtx_pro_6000_server",
             "device_alias": [
-                "rtx_pro_6000_server",
                 "RTX_PRO_6000_SERVER",
                 "rtx6000_server",
             ],
